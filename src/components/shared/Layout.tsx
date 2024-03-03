ce7dc0327e06a54778d1dfafb09204cc26457e76
kambhani <63814718+kambhani@users.noreply.github.com>
2024-03-02 21:22:16 -0500

    Rework routes

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -1,18 +1,16 @@
-import { ReactNode } from "react";
+import { Link, Outlet } from "react-router-dom";
 
-type Props = {
-	children?: ReactNode | undefined;
-};
-
-export default function Layout(props: Props) {
+export default function Layout() {
 	return (
 		<div className="min-h-screen bg-black p-4 text-white">
 			<div className="mx-auto w-fit">
-				<a href="/">
+				<Link to="/">
 					<img src="/images/chenflix.png" alt="Chenflix" className="w-40" />
-				</a>
+				</Link>
 			</div>
-			<div className="mx-auto mt-8 w-full max-w-7xl">{props.children}</div>
+			<div className="mx-auto mt-8 w-full max-w-7xl">
+				<Outlet />
+			</div>
 		</div>
 	);
 }
