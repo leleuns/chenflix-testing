9ade74b251998d9143dcc985df2abaa5cc5108e3
Lawrence Smith <106993743+leleuns@users.noreply.github.com>
2024-03-11 00:39:12 -0700

    not perfect, need to add stuff back, missing shadow

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -2,13 +2,8 @@ import { Link, Outlet } from "react-router-dom";
 
 export default function Layout() {
 	return (
-		<div className="min-h-screen bg-black p-4 text-white">
-			<div className="mx-auto w-fit">
-				<Link to="/">
-					<img src="/images/chenflix.png" alt="Chenflix" className="w-40" />
-				</Link>
-			</div>
-			<div className="mx-auto mt-8 w-full max-w-7xl">
+		<div >
+			<div >
 				<Outlet />
 			</div>
 		</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
-import CoursePreview from "../components/CoursePreview";
-import { courses } from "../coursedata/courses";
+// import CoursePreview from "../components/CoursePreview";
+// import { courses } from "../coursedata/courses";
+import Capybara from "../three/capy";
+
 
 export default function Home() {
 	return (
 		<div>
-			<div className="mx-auto grid w-full grid-cols-1 gap-10 md:grid-cols-2">
-				{courses.map((course) => (
-					<CoursePreview key={course.id} course={course} />
-				))}
-			</div>
+            <Capybara/>
 		</div>
 	);
 }
