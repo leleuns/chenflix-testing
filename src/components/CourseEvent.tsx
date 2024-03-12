ba2b51a96d38f2a1407b0937ac10528be2ae629a
farbs03 <cgeraldfarber@icloud.com>
2024-03-11 20:25:53 -0400

    Update CourseEvent.tsx

--- src/components/CourseEvent.tsx
+++ src/components/CourseEvent.tsx
@@ -19,12 +19,12 @@ export default function CourseEvent({ event }: { event: CourseEvent }) {
 	);
 
 	return (
-		<div className="mx-auto flex w-full flex-col justify-between gap-2 p-4 text-left md:flex-row md:items-start md:gap-4">
+		<div className="mx-auto flex w-full flex-col justify-between gap-2 p-2 text-left md:flex-row md:items-start md:gap-4">
 			<div className="h-[20px] min-w-[80px] text-sm font-semibold">
 				<p className="md:py-0.5">{event.date}</p>
 			</div>
 			<div className="flex flex-grow flex-col gap-2">
-				<div className="flex items-center justify-between">
+				<div className="flex flex-col justify-between gap-2 md:flex-row md:items-center">
 					<div className="flex items-center gap-2">
 						<div className="flex shrink-0 grow-0 flex-col items-center justify-center align-baseline">
 							<input
@@ -36,57 +36,65 @@ export default function CourseEvent({ event }: { event: CourseEvent }) {
 						</div>
 						<p>{event.description}</p>
 					</div>
-					<button className="ml-1 w-fit">
-						<StarIcon
-							className="h-5 w-5"
-							style={{ color: starred ? "#d0ba92" : "#f0f0f0" }}
-							onClick={() => setStarred(!starred)}
-							id={event.description + event.date + "_star"}
-						/>
-					</button>
-				</div>
-				<div className="flex items-center space-x-2">
-					<div
-						className={`group grid aspect-square w-7 place-items-center rounded-md bg-white/10 ${
-							event.videos
-								? "transition duration-100 ease-in hover:text-red-400 hover:ring-2 hover:ring-red-600"
-								: "cursor-not-allowed opacity-70"
-						} `}
-					>
-						{event.videos ? (
-							<a
-								className="flex h-full w-full items-center justify-center"
-								href={event.videos ? event.videos : "#"}
-							>
+					<div className="flex items-center gap-2">
+						<div
+							className={`group grid aspect-square w-7 place-items-center rounded-md bg-white/10 ${
+								event.videos
+									? "transition duration-100 ease-in hover:text-red-400 hover:ring-2 hover:ring-red-600"
+									: "cursor-not-allowed opacity-70"
+							} `}
+						>
+							{event.videos ? (
+								<a
+									className="flex h-full w-full items-center justify-center"
+									href={event.videos ? event.videos : "#"}
+									target="_blank"
+								>
+									<VideoCameraIcon className={`h-4 w-4`} />
+								</a>
+							) : (
 								<VideoCameraIcon className={`h-4 w-4`} />
-							</a>
-						) : (
-							<VideoCameraIcon className={`h-4 w-4`} />
-						)}
-					</div>
-					<div>
-						<a
-							href={event.notes ? event.notes : "#"}
+							)}
+						</div>
+						<div
 							className={`group grid aspect-square w-7 place-items-center rounded-md bg-white/10 ${
 								event.notes
 									? "transition duration-100 ease-in hover:text-red-400 hover:ring-2 hover:ring-red-600"
 									: "cursor-not-allowed opacity-70"
 							} `}
 						>
-							<DocumentIcon className="h-4 w-4" />
-						</a>
-					</div>
-					{event.boilerexams && (
-						<div className="group grid aspect-square w-7 place-items-center rounded-md bg-white/10 transition duration-100 hover:ring-2 hover:ring-white">
-							<a href={event.boilerexams}>
-								<img
-									className="w-5 group-hover:opacity-70"
-									src="/images/BXlogoSubscript.png"
-									alt="Logo"
-								/>
-							</a>
+							{event.notes ? (
+								<a
+									className="flex h-full w-full items-center justify-center"
+									href={event.notes ? event.notes : "#"}
+									target="_blank"
+								>
+									<DocumentIcon className={`h-4 w-4`} />
+								</a>
+							) : (
+								<DocumentIcon className={`h-4 w-4`} />
+							)}
 						</div>
-					)}
+						{event.boilerexams && (
+							<div className="group grid aspect-square w-7 place-items-center rounded-md bg-white/10 transition duration-100 hover:ring-2 hover:ring-white">
+								<a href={event.boilerexams} target="_blank">
+									<img
+										className="w-5 group-hover:opacity-70"
+										src="/images/BXlogoSubscript.png"
+										alt="Logo"
+									/>
+								</a>
+							</div>
+						)}
+						<button>
+							<StarIcon
+								className={`h-5 w-5 transition duration-100 ease-in ${!starred ? "rotate-0 text-white" : "rotate-[72deg] text-yellow-400"}`}
+								// style={{ color: starred ? "#d0ba92" : "#f0f0f0" }}
+								onClick={() => setStarred(!starred)}
+								id={event.description + event.date + "_star"}
+							/>
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
