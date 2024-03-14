07b5b4bd1dde6a1b7fd840a30c310044933a89f4
Sonal Narasimha <narasim9@purdue.edu>
2024-03-14 17:55:56 -0400

    added images to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,13 @@ export default function Footer() {
 			<div className="flex items-center justify-center border-b-2 border-neutral-200 p-2">
 				<div>				
           			<a href="https://www.math.purdue.edu/~chenjk/" className="font-semibold mb-3">Chenflix Original</a>
-					<div>
-						{/*<a href="https://github.com/boilerexams/chenflix" className="mb-3">
-							<img src="https://github.com/primer/brand/assets/19292210/eea6f831-4aa3-4cd2-aa08-a27c03ab0e30" alt="Github Repository" className="w-10 ml-12"/>
-	</a>*/}
-						<img src="/images/BXlogoSubscript.png" alt="Made by BoilerExams" className="mb-1 w-8"/>
+					<div className="flex items-center">
+						<a  target="_blank" href="https://github.com/boilerexams/chenflix" className="mb-3">
+							<img src="/public/footer/github-mark-white.png" className="w-8"/>
+	</a>
+						<div>
+							<img src="/public/footer/BoilerExamsLogoWHITE.png" alt="Made by BoilerExams" className="ml-12 mb-1 w-24"/>
+						</div>
 					</div>
 					<p className="p-10 text-xs mb-4 ">Copyright &copy; Chenflix 2024</p>
 				</div>
