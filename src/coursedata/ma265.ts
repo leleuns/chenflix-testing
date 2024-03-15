3ba609e9f926f0dcb905640ec989f5885a802abd
kambhani <63814718+kambhani@users.noreply.github.com>
2024-03-14 22:15:58 -0400

    Start adding ma265

--- src/coursedata/ma265.ts
+++ src/coursedata/ma265.ts
@@ -1,8 +1,104 @@
+// All course data is from 2018
+
 export const ma265_data: Course = {
 	id: 4,
 	path: "/ma265",
 	abbreviation: "ma26500",
 	name: "MA 26500",
 	description: "Linear Algebra",
-	events: [],
+	events: [
+		{
+			description: "1.1 — Systems of Linear Equations)",
+			date: "Mon 6/11",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer+2018+-+MA265+-+Chen+6-11-18/1_p6mqa6d4/94007531",
+			notes: "/notes/ma265/Lesson1.pdf",
+		},
+		{
+			description: "1.2 — Row Reduction and Echelon Forms",
+			date: "Tues 6/12",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer+2018+-+MA265+-+Chen/1_n3vp3nbe/94007531",
+			notes: "/notes/ma265/Lesson2.pdf",
+		},
+		{
+			description: "1.3 — Vector Equations",
+			date: "Wed 6/13",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer+2018+-+MA265+-+Chen/1_fce48e7o/94007531",
+			notes: "/notes/ma265/Lesson3.pdf",
+		},
+		{
+			description:
+				"1.4 & 1.5 — The Matrix Equation Ax = b & Solution Sets of Linear Systems",
+			date: "Thurs 6/14",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_xuw28dmj/94007531",
+			notes: "/notes/ma265/Lesson4.pdf",
+		},
+		{
+			description:
+				"1.7 & 1.8 — Linear Independence & Introduction to Linear Transformations",
+			date: "Fri 6/15",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_bxa2v42f/94007531",
+			notes: "/notes/ma265/Lesson5.pdf",
+		},
+		{
+			description: "1.9 — The Matrix of Linear Transformation",
+			date: "Mon 6/18",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_6ndt4jo8/94007531",
+			notes: "/notes/ma265/Lesson6.pdf",
+		},
+		{
+			description: "2.1 — Matrix Operations",
+			date: "Tues 6/19",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_wz09u38j/94007531",
+			notes: "/notes/ma265/Lesson7.pdf",
+		},
+		{
+			description: "2.2 & 2.3 — The Inverse of a Matrix",
+			date: "Wed 6/20",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_qlg0jozz/94007531",
+			notes: "/notes/ma265/Lesson8.pdf",
+		},
+		{
+			description: "2.8 — Subspaces of R^n",
+			date: "Thurs 6/21",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_a073ecvf/94007531",
+			notes: "/notes/ma265/Lesson9.pdf",
+		},
+		{
+			description: "2.9 — Dimension and Rank",
+			date: "Fri 6/22",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_5nmpvftv/94007531",
+			notes: "/notes/ma265/Lesson10.pdf",
+		},
+		{
+			description: "Exam 1 Review",
+			date: "Mon 6/25",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_2zvvodau/94007531",
+			notes: "/notes/ma265/E1Review.pdf",
+		},
+		{
+			description: "3.1 & 3.2 — Determinants",
+			date: "Wed 6/27",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_cu3gobao/94007531",
+			notes: "/notes/ma265/Lesson11.pdf",
+		},
+		{
+			description: "3.3 — Cramer's Rule, Volume, and Linear Transformations",
+			date: "Thurs 6/28",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/1_xwfsb0r7/94007531",
+			notes: "/notes/ma265/Lesson12.pdf",
+		},
+	],
 };
