e9da30e32b8d28d39cce9f904f0b3f1f70d7feb8
kambhani <63814718+kambhani@users.noreply.github.com>
2024-03-14 22:29:37 -0400

    Add more events to MA265

--- src/coursedata/ma265.ts
+++ src/coursedata/ma265.ts
@@ -100,5 +100,69 @@ export const ma265_data: Course = {
 				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/1_xwfsb0r7/94007531",
 			notes: "/notes/ma265/Lesson12.pdf",
 		},
+		{
+			description: "4.1 — Vector Spaces and Subspaces",
+			date: "Fri, 6/29",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/1_frtwq6vg/94007531",
+			notes: "/notes/ma265/Lesson13.pdf",
+		},
+		{
+			description: "4.2 — Null Space, Column Space, and Linear Transformations",
+			date: "Mon 7/2",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_hiisn0ct/94007531",
+			notes: "/notes/ma265/Lesson14.pdf",
+		},
+		{
+			description: "4.3 — Linear Independence Sets and Bases",
+			date: "Tues 7/3",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_qjcrhqen/94007531",
+			notes: "/notes/ma265/Lesson15.pdf",
+		},
+		{
+			description: "4.5 — The Dimension of a Vector Space",
+			date: "Thurs 7/5",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_3hx8f3cj/94007531",
+			notes: "/notes/ma265/Lesson16.pdf",
+		},
+		{
+			description: "4.6 — Rank",
+			date: "Fri 7/6",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_fyindmud/94007531",
+			notes: "/notes/ma265/Lesson17.pdf",
+		},
+		{
+			description:
+				"5.1 & 5.2 — Eigenvectors, Eigenvalues, and Characteristic Equations",
+			date: "Mon 7/9",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_uh06h6j8/94007531",
+			notes: "/notes/ma265/Lesson18.pdf",
+		},
+		{
+			description: "5.3 — Diagonalization (Part 1)",
+			date: "Tues 7/10",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_4lm91hc7/94007531",
+			notes: "/notes/ma265/Lesson19.pdf",
+		},
+		{
+			description: "5.3 — Diagonalization (Part 2)",
+			date: "Wed 7/11",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_yc859f33/94007531",
+			notes: "/notes/ma265/Lesson20.pdf",
+		},
+		{
+			description: "Exam 2 Review",
+			date: "Thurs 7/12",
+			videos:
+				"https://mediaspace.itap.purdue.edu/media/Summer-2018+-+MA265+-+Chen/0_v85yk2oz/94007531",
+			notes: "/notes/ma265/E2Review.pdf",
+		},
 	],
 };
