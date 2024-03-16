0639a9ebc4d8ffa079724fcaf0db9d10dde4b295
kambhani <63814718+kambhani@users.noreply.github.com>
2024-03-16 16:40:40 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,24 +2,11 @@ import Footer from "~/components/Footer";
 import CoursePreview from "../components/CoursePreview";
 import { courses } from "../coursedata/courses";
 import Capybara from "../three/capy";
-import { useState, useEffect } from 'react';
 
 export default function Home() {
-	const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth <= 1025);
-
-	useEffect(() => {
-		const handleResize = () => {
-		setIsLargeScreen(window.innerWidth <= 1025);
-		};
-
-		window.addEventListener('resize', handleResize);
-		return () => window.removeEventListener('resize', handleResize);
-	}, []);
-	
 	return (
-		<>
+		<div className="bg-black">
 			<div className="min-h-screen bg-black text-white">
-
 				<Capybara />
 				<img
 					className="absolute left-1/2 top-4 w-40 -translate-x-1/2"
@@ -30,12 +17,11 @@ export default function Home() {
 					{courses.map((course) => (
 						<CoursePreview key={course.id} course={course} />
 					))}
-				</div>			
-				<div className={`absolute w-full p-4 pt-32 ${isLargeScreen ? 'bg-black' : ''}`}>
+				</div>
+				<div className="w-full p-4 pt-32 lg:absolute">
 					<Footer />
 				</div>
 			</div>
-
-		</>
+		</div>
 	);
 }
