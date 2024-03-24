ecf16c702a76c4912910ce82f589a6114fcdb290
farbs03 <cgeraldfarber@icloud.com>
2024-03-24 15:20:21 -0400

    course transfer for 161 and 261 complete

--- src/coursedata/ma161.ts
+++ src/coursedata/ma161.ts
@@ -6,13 +6,256 @@ export const ma161_data: Course = {
 	description: "Plane Analytic Geometry And Calculus I",
 	events: [
 		{
-			description:
-				"Lesson 0 (1.1 Review of Functions and 1.2 Representing Functions)",
 			date: "Mon 1/10",
-			videos:
-				"https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_9p8gxlsg",
-			notes:
-				"https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%200%20%281.1,%201.2%29.pdf",
+			description: "Lesson 0 (1.1 Review of Functions and 1.2 Representing Functions)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_9p8gxlsg",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%200%20%281.1,%201.2%29.pdf"
 		},
-	],
+		{
+			date: "Wed 1/12",
+			description: "Lesson 1 (1.3 Exponential and Logarithmic Functions)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_r0t6y7pn",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%201%20%281.3%29.pdf"
+		},
+		{
+			date: "Fri 1/14",
+			description: "Lesson 2 (1.4 Trigonometric Functions and Their Inverses)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_0y7pj55p",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%202%20%281.4%29.pdf"
+		},
+		{
+			date: "Wed 1/19",
+			description: "Lesson 3 (2.1: The Idea of Limits and 2.2 Definition of Limits)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_8ysqm23m",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%203%20%282.1+2.2%29.pdf"
+		},
+		{
+			date: "Fri 1/21",
+			description: "Lesson 4 (2.3 Techniques for Computing Limits)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_2r3xrnmw",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%204%20%282.3%29.pdf"
+		},
+		{
+			date: "Mon 1/24",
+			description: "Lesson 5 (2.4 Infinite Limits)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_nqdbon20",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%205%20%282.4%29.pdf"
+		},
+		{
+			date: "Wed 1/26",
+			description: "Lesson 6 (2.5 Limits at Infinity)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_0xj7a2al",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%206%20%282.5%29.pdf"
+		},
+		{
+			date: "Fri 1/28",
+			description: "Lesson 7 (2.6 Continuity)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_5m8vi1rr",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%207%20%282.6%29.pdf"
+		},
+		{
+			date: "Mon 1/31",
+			description: "Exam 1 Review",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_a4jnpeu3",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Exam%201%20Review.pdf"
+		},
+		{
+			date: "Wed 2/2",
+			description: "Lesson 8 (3.1 Introducing the Derivative)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_worroizy",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%208%20%283.1%29.pdf"
+		},
+		{
+			date: "Mon 2/7",
+			description: "Lesson 9 (3.2 The Derivative as a Function)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_b3wi6okj",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%209%20%283.2%29.pdf"
+		},
+		{
+			date: "Wed 2/9",
+			description: "Lesson 10 (3.3 Rules of Differentiation)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_c87cy0ad",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2010%20%283.3%29.pdf"
+		},
+		{
+			date: "Fri 2/11",
+			description: "Lesson 11 (3.4 The Product and Quotient Rules)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_dc0ooiiz",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2011%20%283.4%29.pdf"
+		},
+		{
+			date: "Mon 2/14",
+			description: "Lesson 12 (3.5 Derivatives of Trigonometric Functions)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_pxdkw50c",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2012%20%283.5%29.pdf"
+		},
+		{
+			date: "Wed 2/16",
+			description: "Lesson 13 (3.6 Derivatives as Rates of Change and 3.7 The Chain Rule, part 1)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_uhcx9y2r",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2013%20%283.6+3.7-1%29.pdf"
+		},
+		{
+			date: "Fri 2/18",
+			description: "Lesson 14 (3.7 The Chain Rule, part 2)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_v6ugufio",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2014%20%283.7-2%29.pdf"
+		},
+		{
+			date: "Mon 2/21",
+			description: "Lesson 15 (3.8 Implicit Differentiation)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_fpqobgq1",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2015%20%283.8%29.pdf"
+		},
+		{
+			date: "Wed 2/23",
+			description: "Lesson 16 (3.9 Derivatives of Logarithmic and Exponential Functions)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_6i9tmqx9",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2016%20%283.9%29.pdf"
+		},
+		{
+			date: "Fri 2/25",
+			description: "Lesson 17 (3.10 Derivatives of Inverse Trigonometric Functions)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_ek1tcnxl",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2017%20%283.10%29.pdf"
+		},
+		{
+			date: "Mon 2/28",
+			description: "Exam 2 Review",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_bksv2s6n",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Exam%202%20Review.pdf"
+		},
+		{
+			date: "Wed 3/2",
+			description: "Lesson 18 (3.11 Related Rates, part 1)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_7t81asr9",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2018%20%283.11-1%29.pdf"
+		},
+		{
+			date: "Fri 3/4",
+			description: "Lesson 19 (3.11 Related Rates, part 2)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_rowwv8ue",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2019%20%283.11-2%29.pdf"
+		},
+		{
+			date: "Mon 3/7",
+			description: "Lesson 20 (4.1 Maxima and Minima)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_rnpzqx6i",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2020%20%284.1%29.pdf"
+		},
+		{
+			date: "Wed 3/9",
+			description: "Lesson 21 (4.2 The Mean Value Theorem and 4.3 What Derivatives Tell us, part 1)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_m0niu6r1",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2021%20%284.2+4.3-1%29.pdf"
+		},
+		{
+			date: "Mon 3/21",
+			description: "Lesson 22 (4.3 What Derivatives Tell us, part 2)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_od1k1r6a",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2022%20%284.3-2%29.pdf"
+		},
+		{
+			date: "Wed 3/23",
+			description: "Lesson 23 (4.4 Graphing Functions, part 1)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_uwqgk89i",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2023%20%284.4-1%29.pdf"
+		},
+		{
+			date: "Fri 3/25",
+			description: "Lesson 24 (4.4 Graphing Functions, part 2)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_020q6j6r",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2024%20%284.4-2%29.pdf"
+		},
+		{
+			date: "Mon 3/28",
+			description: "Lesson 25 (4.5 Optimization Problems, part 1)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_qrnk39jb",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2025%20%284.5-1%29.pdf"
+		},
+		{
+			date: "Wed 3/30",
+			description: "Lesson 26 (4.5 Optimization Problems, part 2)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_vakqp0ms",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2026%20%284.5-2%29.pdf"
+		},
+		{
+			date: "Fri 4/1",
+			description: "Lesson 27 (4.6 Linear Approximation)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_d0yz894p",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2027%20%284.6%29.pdf"
+		},
+		{
+			date: "Mon 4/4",
+			description: "Lesson 28 (4.7 l'Hospital's Rule)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_g0hpau07",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2028%20%284.7%29.pdf"
+		},
+		{
+			date: "Wed 4/6",
+			description: "Lesson 29 (4.9 Antiderivatives)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_pa6qrdot",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2029%20%284.9%29.pdf"
+		},
+		{
+			date: "Fri 4/8",
+			description: "Lesson 30 (5.1 Approximating Areas Under Curves)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_r97imn5o",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2030%20%285.1%29.pdf"
+		},
+		{
+			date: "Mon 4/11",
+			description: "Exam 3 Review",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_zcc7rlum",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Exam%203%20Review.pdf"
+		},
+		{
+			date: "Wed 4/13",
+			description: "Lesson 31 (5.2 Definite Integrals)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_5usec61j",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2031%20%285.2%29.pdf"
+		},
+		{
+			date: "Fri 4/15",
+			description: "Lesson 32 (5.3 Fundamental Theorem of Calculus)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_7p4y3gx8",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2032%20%285.3%29.pdf"
+		},
+		{
+			date: "Mon 4/18",
+			description: "Lesson 33 (5.4 + 5.5-1 Working with Integrals and Substitution Rule)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_sy3yz8v5",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2033%20%285.4+5.5-1%29.pdf"
+		},
+		{
+			date: "Wed 4/20",
+			description: "Lesson 34 (5.5-2 Substitution Rule, part 2)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_og8ykibn",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2034%20%285.5-2%29.pdf"
+		},
+		{
+			date: "Fri 4/22",
+			description: "Lesson 35 (7.2 Exponential Growth and Decay)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_ls3fstln",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Lesson%2035%20%287.2%29.pdf"
+		},
+		{
+			date: "Mon 4/25",
+			description: "Final Exam Review (part 1)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_l54j7fop",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Final%20Review%201.pdf"
+		},
+		{
+			date: "Wed 4/27",
+			description: "Final Exam Review (part 2)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_2v5jurxl",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Final%20Review%202.pdf"
+		},
+		{
+			date: "Fri 4/29",
+			description: "Final Exam Review (part 3)",
+			videos: "https://mediaspace.itap.purdue.edu/media/Spring+2022+-+MA161+-+Chen/1_9j01e1jg",
+			notes: "https://www.math.purdue.edu/~chenjk/MA161S22/Final%20Review%203.pdf"
+		}
+	]
 };
