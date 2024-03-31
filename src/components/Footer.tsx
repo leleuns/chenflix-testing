fe7385a1025a9e2e5eafa1d6af682d917ba16bd3
Anish Kambhampati <63814718+kambhani@users.noreply.github.com>
2024-03-30 23:39:43 -0400

    Update footer (#11)
    * added chen's email and changed signature line
    
    * Update Footer.tsx
    
    ---------
    
    Co-authored-by: Sonal Narasimha <narasim9@purdue.edu>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,24 @@
 export default function Footer() {
 	return (
 		<footer className="z-20 mt-10 w-full pt-6 text-center text-stone-300">
-			<div className="flex items-center justify-center p-2 text-xl">
+			<div className="flex items-center justify-center p-1 text-xl">
 				<a
 					href="https://www.math.purdue.edu/~chenjk/"
-					className="mb-3 font-semibold ease-in hover:opacity-80"
+					className="mb-1 font-semibold ease-in hover:opacity-80"
 				>
 					Chenflix Original
 				</a>
 			</div>
-			<div className="flex items-center justify-center pt-3">
-				<img
-					src="/footer/BoilerExamsLogoWHITE.png"
-					alt="Made by Boilerexams"
-					className="mb-1 w-28"
-				/>
+			<div className="flex items-center justify-center pt-1">
+				<p className="mb-1">A</p>
+				<a target="_blank" href="https://www.boilerexams.com/">
+					<img
+						src="/footer/BoilerExamsLogoWHITE.png"
+						alt="Made by Boilerexams"
+						className="w-28 p-2"
+					/>
+				</a>
+				<p className="mb-1">Project</p>
 			</div>
 			<div className="flex items-center justify-center pt-3">
 				<a
@@ -25,8 +29,14 @@ export default function Footer() {
 					<img src="/footer/github-mark-white.png" className="w-8" />
 				</a>
 			</div>
+			<div className="flex items-center justify-center pt-2">
+				Support:
+				<a className="ml-1" href="mailto:chenjk@purdue.edu">
+					chenjk@purdue.edu
+				</a>
+			</div>
 			<div>
-				<p className="mb-3 p-6 text-sm ">Copyright &copy; 2024 Chenflix</p>
+				<p className="mb-3 p-5 text-sm ">Copyright &copy; 2024 Chenflix</p>
 			</div>
 		</footer>
 	);
